ad672b82177d33928add138c0a14465e98822564
Glucon <ombindraindode@proton.me>
2025-08-16 09:48:58 +0800

    feat: add lammps-apt

---
+++ lammps-apt/build.sh
@@ -0,0 +1,3 @@
+unset no_proxy ftp_proxy http_proxy https_proxy all_proxy rsync_proxy
+unset NO_PROXY FTP_PROXY HTTP_PROXY HTTPS_PROXY ALL_PROXY RSYNC_PROXY
+apptainer build lammps-apt.sif lammps-apt.def
