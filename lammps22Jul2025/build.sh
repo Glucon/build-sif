8c5aa21b1c6b066f19cecf15a76a2bd1f4600ead
Glucon <ombindraindode@proton.me>
2025-08-17 10:03:43 +0800

    chore(lmp): rename lammps22Jul2025

--- lammps22Jul2025/build.sh
+++ lammps22Jul2025/build.sh
@@ -1,3 +1,3 @@
 unset no_proxy ftp_proxy http_proxy https_proxy all_proxy rsync_proxy
 unset NO_PROXY FTP_PROXY HTTP_PROXY HTTPS_PROXY ALL_PROXY RSYNC_PROXY
-apptainer build lammps-22Jul2025.sif lammps-22Jul2025.def
+apptainer build lammps22Jul2025.sif lammps22Jul2025.def
