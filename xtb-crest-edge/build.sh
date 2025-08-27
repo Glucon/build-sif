fddee6f76d3177cdb9f6ce5473ba5c07c3b77978
Glucon <ombindraindode@proton.me>
2025-08-27 21:52:19 +0800

    fix: change typo in #!/usr/bin/env bash

--- xtb-crest-edge/build.sh
+++ xtb-crest-edge/build.sh
@@ -1,4 +1,4 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 unset no_proxy ftp_proxy http_proxy https_proxy all_proxy rsync_proxy
 unset NO_PROXY FTP_PROXY HTTP_PROXY HTTPS_PROXY ALL_PROXY RSYNC_PROXY
 sudo apptainer build xtb-crest-edge.sif xtb-crest-edge.def
