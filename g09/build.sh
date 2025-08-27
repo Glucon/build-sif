3e893f99618da4ba35274aede8c4af274583b75e
Glucon <ombindraindode@proton.me>
2025-08-27 21:40:56 +0800

    chore: add sudo and /usr/bin/env bash

--- g09/build.sh
+++ g09/build.sh
@@ -1,3 +1,4 @@
+#!usr/bin/env bash
 unset no_proxy ftp_proxy http_proxy https_proxy all_proxy rsync_proxy
 unset NO_PROXY FTP_PROXY HTTP_PROXY HTTPS_PROXY ALL_PROXY RSYNC_PROXY
-apptainer build g09.sif g09.def
+sudo apptainer build g09.sif g09.def
--- lammps-apt/build.sh
+++ lammps-apt/build.sh
@@ -1,3 +1,4 @@
+#!usr/bin/env bash
 unset no_proxy ftp_proxy http_proxy https_proxy all_proxy rsync_proxy
 unset NO_PROXY FTP_PROXY HTTP_PROXY HTTPS_PROXY ALL_PROXY RSYNC_PROXY
-apptainer build lammps_apt.sif lammps_apt.def
+sudo apptainer build lammps_apt.sif lammps_apt.def
